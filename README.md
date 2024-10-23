6a0dd1b634f962fb430984c049026380ca33acf7
Ajay jadhav <ajayjadhav0913@gmail.com>
2024-10-23 15:09:39 +0530

    creating small server

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # node_js
 node.js assignment  questions   
+
+// node js first  task
+// GET
+// POST
+// DELETE
+//UPDATE
--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 /*
 Problem 1: NPM and Package.json
-You're starting a new project and want to manage your project's dependencies using NPM. Explain the purpose of NPM and how it helps in managing packages. Create a simple package.json file for your project, specifying the name, version, and a few dependencies of your choice.
+Y Explain the purpose of NPM and how it helps in managing packages. Create a simple package.json file for your prou're starting a new project and want to manage your project's dependencies using NPM.oject, specifying the name, version, and a few dependencies of your choice.
 
 */
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',function(req ,res){
+    res.send("chai is ready");
+})
+
+app.listen(30001);
 
 
